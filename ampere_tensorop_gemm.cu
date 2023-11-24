a70f2886db7cf0a0889970aee120f013905def17
ybai62868 <ybai62868@gmail.com>
2023-11-24 15:03:19 +0800

    Update ampere_tensorop_gemm.cu

--- ampere_tensorop_gemm.cu
+++ ampere_tensorop_gemm.cu
@@ -51,7 +51,7 @@ struct Options {
   int iterations;
 
   Options()
-      : help(false), problem_size({5120, 4096, 4096}), batch_count(1),
+      : help(false), problem_size({4096, 4096, 4096}), batch_count(1),
         reference_check(true), iterations(200), alpha(1), beta() {}
 
   bool valid() { return true; }
